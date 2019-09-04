5f0b011cb11285e9ce1416ce723280d5765af96a
Julian Späth <spaethju@posteo.de>
2019-09-04 19:09:28 +0200

    Initial commit

---
+++ label_creator.py
@@ -0,0 +1,17 @@
+from download_helper import download_donor_summary
+
+
+def extract_survival_labels(token, features):
+    donors = download_donor_summary(token=token, filetype="donor", release=28).set_index("icgc_donor_id")
+    feature_donors = features.index
+    labels = donors.loc[feature_donors, ["donor_survival_time", "donor_vital_status"]]
+    labels["donor_survival_time"] = labels["donor_survival_time"]/365
+    labels["donor_survival_time"] = labels["donor_survival_time"].apply(lambda x: round(x, 2))
+    labels["donor_survival_time"] = labels["donor_survival_time"].astype("float16")
+    labels["donor_vital_status"] = labels["donor_vital_status"].replace({"alive": 0, "deceased": 1})
+    labels = labels.dropna()
+    labels["donor_vital_status"].astype("int8")
+
+    features = features.loc[labels.index, :]
+
+    return labels, features
